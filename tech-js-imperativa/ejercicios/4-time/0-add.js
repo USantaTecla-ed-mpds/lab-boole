c48cce3ac9267aff45df6d345d80ad4237384671
Borja <39737321+borxdev@users.noreply.github.com>
2022-04-27 20:21:18 +0200

    0-add.js: Simplify code

--- tech-js-imperativa/ejercicios/4-time/0-add.js
+++ tech-js-imperativa/ejercicios/4-time/0-add.js
@@ -15,14 +15,13 @@ const seconds2 = console.readNumber('Dame los segundos: ');
 
 const sumSeconds = seconds + seconds2;
 const sumMinutes = minutes + minutes2;
-const sumHours = hours + hours2;
 
 const extraMinutes = sumSeconds < 60 ? 0 : (sumSeconds < 120 ? 1 : 2);
 const extraHours = sumMinutes < 60 ? 0 : (sumMinutes < 120 ? 1 : 2);
 
 const totalSeconds = extraMinutes ? sumSeconds % 60 : sumSeconds;
 const totalMinutes = (extraHours ? sumMinutes % 60 : sumMinutes) + extraMinutes;
-const totalHours = sumHours + extraHours;
+const totalHours = hours + hours2 + extraHours;
 
 const printSeconds = totalSeconds < 10 ? `0${totalSeconds}` : totalSeconds;
 const printMinutes = totalMinutes < 10 ? `0${totalMinutes}` : totalMinutes;
