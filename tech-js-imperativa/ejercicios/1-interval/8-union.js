6145b7edf1207fbf0d47b81774bd25fc02315939
Borja <39737321+borxdev@users.noreply.github.com>
2022-04-28 20:12:46 +0200

    1-interval / 8-union

---
+++ tech-js-imperativa/ejercicios/1-interval/8-union.js
@@ -0,0 +1,16 @@
+const { Console } = require('console-mpds');
+const console = new Console();
+
+// 1-interval / 8-union
+console.writeln('Primer intervalo: ----------');
+const minInterval = console.readNumber('Introduce el mínimo del intervalo: ');
+const maxInterval = console.readNumber('Introduce el máximo del intervalo (superior o igual al mínimo): ');
+
+console.writeln('Segundo intervalo: ----------')
+const minInterval2 = console.readNumber('Introduce el mínimo del intervalo: ');
+const maxInterval2 = console.readNumber('Introduce el máximo del intervalo (superior o igual al mínimo): ');
+
+const intersectionMin = minInterval < minInterval2 ? minInterval : minInterval2;
+const intersectionMax = maxInterval > maxInterval2 ? maxInterval : maxInterval2;
+
+console.writeln(`El intervalo [${minInterval}, ${maxInterval}] intersección con el intervalo [${minInterval2}, ${maxInterval2}] será el intervalo [${intersectionMin}, ${intersectionMax}]`);
