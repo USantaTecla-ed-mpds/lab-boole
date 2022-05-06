8f414a5b5e51612d34fe147ce096951d4a453f10
Borja <39737321+borxdev@users.noreply.github.com>
2022-05-06 17:28:07 +0200

    Improve 1-interval / 4-split-v0: Co-authored-by: David Corrales <Xsalieri@users.noreply.github.com> Co-authored-by: SantiagoQP31 <SantiagoQP31@users.noreply.github.com> Co-authored-by: elfarolas <lfcv00007@users.noreply.github.com>

--- tech-js-estructurada/ejercicios/1-interval/4-split-v0.js
+++ tech-js-estructurada/ejercicios/1-interval/4-split-v0.js
@@ -1,37 +1,42 @@
 const { Console } = require("console-mpds");
 const console = new Console();
 
-
 // 1-interval/4-split-v0
-let minInterval;
+
+let minInterval = console.readNumber(`Introduce el mínimo del intervalo: `);
 let maxInterval;
-let amount;
-let msg;
-while(maxInterval < minInterval || maxInterval === undefined){
-    if(maxInterval < minInterval){
-        console.writeln(`Error!!! El máximo debe ser superior o igual al máximo`);
-    }
-    minInterval = console.readNumber(`Introduce el mínimo del intervalo: `);
-    maxInterval = console.readNumber(`Introduce el máximo del intervalo (superior\
- o igual al mínimo): `);
+let range;
+let steps;
+let newMax;
+let result = '';
+
+while (typeof range !== 'number' || range < 0) {
+  maxInterval = console.readNumber(`Introduce el máximo del intervalo (superior o igual al mínimo): `);
+  range = maxInterval - minInterval;
+
+  if (range < 0) {
+    console.writeln(`\n¡Error! El máximo debe ser superior o igual al mínimo`);
+  }
 }
 
-while(amount <=0 || amount === undefined){
-    if(amount <= 0){
-        console.writeln('Error!!! La cantidad debe ser positiva');
-    }
-    amount = console.readNumber(`Introduce una cantidad positiva de intervalos: `);
+while (!steps || steps < 0) {
+  steps = console.readNumber(`Introduce una cantidad positiva de intervalos: `);
+
+  if (steps <= 0) {
+    console.writeln('\n¡Error! La cantidad de intervalos debe ser positiva');
+  }
 }
-msg = `El intervalo [${minInterval},${maxInterval}] dividido en ${amount}\
- intervalos son )`;
-const length = (maxInterval - minInterval)/amount;
-let newMaxInterval;
-for(i=1;i<=amount;i++){
-    newMaxInterval = minInterval + length;
-    msg += `[${minInterval},${newMaxInterval}]`;
-    if(newMaxInterval < maxInterval){
-        i+1 < amount ? msg +=', ' : msg+=' y ';
-        minInterval = newMaxInterval;
+
+const size = range / steps;
+
+for (let i=1; i<=steps; i++) {
+    newMax = minInterval + size;
+    result += `[${minInterval}, ${newMax}]`;
+
+    if (newMax < maxInterval) {
+        i + 1 < steps ? (result += ', ') : (result += ' y ');
+        minInterval = newMax;
     }
 }
-console.writeln(msg);
+
+console.writeln(`El intervalo [${minInterval}, ${maxInterval}] dividido en ${steps} intervalo${steps>1?'s':''} son: ${result}`);
