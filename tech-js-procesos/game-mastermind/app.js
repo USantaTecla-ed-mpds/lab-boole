9976517c8fe0f5748175e128d9777b8f8b20a62c
Borja <39737321+borxdev@users.noreply.github.com>
2022-05-26 22:00:16 +0200

    Revert & update app.js mastermind

--- tech-js-procesos/game-mastermind/app.js
+++ tech-js-procesos/game-mastermind/app.js
@@ -1,4 +1,4 @@
-/ game mastermind
+// game mastermind
 // process oriented programming
 
 const { Console } = require("console-mpds");
@@ -10,16 +10,20 @@ function playMastermind() {
     do {
         playGame();
     } while (repeatGame());
-    console.writeln('Bye! See you next time');
+    console.writeln('\nBye! See you next time');
 
 
     function playGame() {
         const CODE_LENGTH = 4;
         const VALID_COLORS = ['R', 'G', 'B', 'Y', 'C', 'M'];
-        console.writeln('VALID COLORS: ' + VALID_COLORS.join(', '));
 
-        const secretCode = getSecretCode(CODE_LENGTH);
-        console.writeln(`The secret code is: ${secretCode}`);
+        let secretCode = '';
+        secretCode = getSecretCode(CODE_LENGTH);
+        let secretCodeMsg = '';
+        for (let color of secretCode) {
+            secretCodeMsg += `${color} `;
+        }
+        console.writeln(`\nThe secret code is: [${secretCodeMsg}]\n`);
         
         do {
             console.writeln('Start decoding!');
@@ -31,35 +35,30 @@ function playMastermind() {
 
 
         function getSecretCode(length) {
-            console.writeln(`Hi CodeMaker, please enter a Secret Code with only ${length} colors.
-You can use 1 or ${length} of these colors: [R]ed, [G]reen, [B]lue, [Y]ellow, [C]yan & [M]agenta.`);
-            let secretCode = [];
+            console.writeln(`\
+            \nHi CodeMaker, please enter a Secret Code with only ${length} colors.\
+            \nYou can use 1 or ${length} of these colors: [R]ed, [G]reen, [B]lue, [Y]ellow, [C]yan & [M]agenta.\n`);
 
+            let secretCode = [];
             for (let i = 1; i <= length; i++) {
                 let isValidColor = true;
                 do {
-                    secretCode[i] = console.readString(`- Enter the color ${i}: `);
                     isValidColor = validateColor(secretCode[i], VALID_COLORS);
+                    if (!isValidColor) {
+                        console.writeln('\nInvalid color, please enter one of these colors: R, G, B, Y, C or M.');
+                    }
                 } while (!isValidColor);
             }
             return secretCode;
 
             // functions
             function validateColor(color, validColors) {
-                console.writeln(` ---- validate color: ${color}`);
-                console.writeln(` ---- validColors: ${validColors}`);
-
                 for (let validColor of validColors) {
                     if (color === validColor) {
                         return true;
-                    } else {
-                        console.writeln(color === validColor);
-                        console.writeln(` --- color: ${color}`);
-                        console.writeln(` --- validColor: ${validColor}`);
-                        console.writeln(`Invalid color, please enter one of these valid colors: R, G, B, Y, C or M \n`);
-                        return false;
                     }
                 }
+                return false;
             }
         }
 
@@ -67,22 +66,6 @@ You can use 1 or ${length} of these colors: [R]ed, [G]reen, [B]lue, [Y]ellow, [C
             return bool;
         }
     }
-
-
-    function repeatGame() {
-        let repeat;
-        let validResponse;
-        do {
-            repeat = console.readString("Do you want to play again? (y/n)");
-            validResponse = repeat === "y" || repeat === "n";
-            if (validResponse) {
-                return repeat === "y";
-            } else {
-                console.writeln("\nPlease, enter a valid response");
-            }
-        } while (!validResponse);
-    }
-}
 
 
     function repeatGame() {
