c887d28a14a2ea946f65db4e7dec8db57c1b2447
David Corrales <corralesdj@gmail.com>
2022-06-08 22:46:47 +0200

    función getProposedCombination implementada

--- tech-js-procesos/game-mastermind/app2.js
+++ tech-js-procesos/game-mastermind/app2.js
@@ -6,75 +6,120 @@ const console = new Console();
 playMastermind();
 
 function playMastermind() {
+  do {
+    playGame();
+  } while (isResumed());
+
+  function playGame() {
+    const COMBINATION_LENGTH = 4;
+    const VALID_COLORS = ["r", "g", "b", "y", "c", "m"];
+    const MAX_ATTEMPTS = 5;
+
+    console.writeln(`\n----- MASTERMIND -----\n\n`);
+    const secretCombination = getSecretCombination(
+      COMBINATION_LENGTH,
+      VALID_COLORS
+    );
+    let nAttempt = 0;
+    showAttempMsg(nAttempt);
+    let proposedCombinations = [];
+    let proposedResults = [];
+    let isBrokensecretCombination = false;
     do {
-        playGame();
-    } while (isResumed());
+      
+      proposedCombinations[nAttempt] = getProposedCombination(
+        COMBINATION_LENGTH,
+        VALID_COLORS
+      );
+      proposedResults[nAttempt] = getProposedResults(
+        proposedCombinations[nAttempt],
+        secretCombination
+      );
+      
+    showAttempMsg(nAttempt+1);
+      showBoard(proposedCombinations[nAttempt], proposedResults[nAttempt]);
+      nAttempt++;
+    } while (nAttempt < MAX_ATTEMPTS);
+  }
 
-    function playGame() {
-        const COMBINATION_LENGTH = 4;
-        const VALID_COLORS = ["r", "g", "b", "y", "c", "m"];
-        const MAX_ATTEMPTS = 5;
+  function showAttempMsg(nAttempt) {
+    console.writeln(`\n${nAttempt} intento (s):`);
+  }
 
-        console.writeln(`\n----- MASTERMIND -----\n\n`);
-        const secretCombination = getSecretCombination(COMBINATION_LENGTH, VALID_COLORS);
-        let nAttempt = 0;
-        showAttempMsg(nAttempt);
-        let proposedCombinations = [];
-        let proposedResults = [];
-        let isBrokensecretCombination = false;
-        do {
-            nAttempt++;
-            proposedCombinations[nAttempt] = getProposedCombination(COMBINATION_LENGTH,VALID_COLORS);
-            proposedResults[nAttempt] = getProposedResults(proposedCombinations[nAttempt],secretCombination);
-            console.writeln(proposedResults[nAttempt]);
-           
-            
-            showAttempMsg(nAttempt);
-            showBoard(proposedCombinations[nAttempt], proposedResults[nAttempt]);
-        } while (nAttempt < MAX_ATTEMPTS);
+  function getSecretCombination(lenght, validColors) {
+    return "rgby";
+  }
 
+  function getProposedCombination(combinationLenght, validColors) {
+    let proposedCombination = "";
+    let isValidCombination, isValidLength;
+    do {
+      proposedCombination = console.readString("Propón una combinación:");
+      isValidCombination = isValidColor(validColors, proposedCombination);
+      isValidLength = proposedCombination.length === combinationLenght;
+      if (!isValidLength) {
+        console.writeln(
+          `La longitud de la combinación propuesta es incorrecta`
+        );
+      } else if (!isValidCombination) {
+        console.writeln(`Colores incorrectos, deben ser: rgybmc`);
+      }
+    } while (!isValidLength || !isValidCombination);
+    return proposedCombination;
+
+      function isValidColor(validColors, proposedCombination) {
+        let numberOfValids = 0;
+        for (const proposedColor of proposedCombination) {
+          for (let i = 0; i < validColors.length; i++) {
+            if (proposedColor === validColors[i]) {
+              numberOfValids++;
+            }
+          }
+        }
+        return numberOfValids === proposedCombination.length;
+      }
+  }
+
+  function getProposedResults(proposedCombination, secretCombination) {
+    let blacks = 0;
+    let whites = 0;
+
+    for (let i = 0; i < proposedCombination.length; i++) {
+      let match = false;
+      for (let j = 0; !match && j < secretCombination.length; j++) {
+        if (proposedCombination[i] === secretCombination[j]) {
+          match = true;
+          if (i === j) {
+            blacks++;
+          } else {
+            whites++;
+          }
+        }
+      }
     }
+    return [blacks, whites];
+  }
 
-    function showAttempMsg(nAttempt){
-        console.writeln(`${nAttempt} intento (s):`)
-    }
+  function showBoard(proposedCombination, result) {
+    const blanksIndex = 0;
+    const whiteIndex = 1;
+    console.writeln(
+      `${proposedCombination} --> ${result[blanksIndex]} blanks and ${result[whiteIndex]} whites`
+    );
+  }
 
-    function getSecretCombination(lenght, validColors){
-        return 'rgby';
-    }
-
-    function getProposedCombination(lenght, validColors){
-        return 'rgcm';
-    }
-    
-    function getProposedResults(proposedCombination,secretCombination){
-        let blacks = 0;
-        let whites = 0;
-
-        for (let i = 0; i < proposedCombination) {
-            let match = false;
-            for (let j = 0; !match && j < secretCombination.length; j++) {
-                if (proposedCombination[i] === secretCombination[j]) {
-                    match = true;
-                    if(i === j){
-                        blacks ++;
-                    }else{
-                        whites ++;
-                    }
-                }
-            }    
-        
-        }    
-        return [blacks,whites];
-    }
-
-    function showBoard(proposedCombination,result){
-        const blanksIndex = 0;
-        const whiteIndex = 1;
-        console.writeln(`${proposedCombination} --> ${result[blanksIndex]} blanks and ${result[whiteIndex]} whites`);
-    }
-
-    function isResumed(){
-        return false;
-    }
+  function isResumed() {
+    let answer;
+    let error;
+    do {
+      answer = console.readString("Quieres jugar otra vez? (s/n)");
+      error = answer !== "s" && answer !== "n";
+      if (error) {
+        console.writeln("\nPor favor, introduce una respuesta correcta.");
+      } else if (answer === "n") {
+        console.writeln("\nFin del juego, hasta pronto!");
+      }
+    } while (error);
+    return answer === "s";
+  }
 }
